67563017118d03a50b572fb5c58069f2bc0bb378
Natasha <natasha.bragg12@gmail.com>
2020-11-13 16:11:03 -0700

    Junit Test

--- JPANewParent/src/main/java/com/skilldistillery/newparent/entities/Baby.java
+++ JPANewParent/src/main/java/com/skilldistillery/newparent/entities/Baby.java
@@ -1,5 +1,83 @@
 package com.skilldistillery.newparent.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Baby {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private int age;
+
+
+	public Baby() {
+		super();
+	}
+	public Baby(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Baby other = (Baby) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Baby [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
+	
 
 }
