98476e9fe20f88d59e056bb38159be80963b616c
Natasha <natasha.bragg12@gmail.com>
2020-11-13 16:41:59 -0700

    Linked projects

---
+++ NewParentTracker/src/main/java/com/skilldistillery/newparent/NewParentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.newparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewParentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewParentTrackerApplication.class, args);
+	}
+
+}
---
+++ NewParentTracker/src/test/java/com/skilldistillery/newparent/NewParentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.newparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewParentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
